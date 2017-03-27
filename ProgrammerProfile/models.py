857459c4257daf973b4c209afca1dc5fc66a46a8
pythongiant <srihari.unnikrishnan@gmail.com>
2017-03-27 18:02:19 +0530

    Change Model

--- ProgrammerProfile/models.py
+++ ProgrammerProfile/models.py
@@ -1,7 +1,16 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from time import time
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Person(models.Model):
+        Name = models.ForeignKey(User)
+        DOB = models.DateField()
+        age = models.IntegerField()
+        Description = models.TextField()
+        Profile_Picture = models.ImageField(upload_to="/static")
+
 
