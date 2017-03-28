b59506b82ec5ecb991c236aa628e07d3a714ee90
pythongiant <srihari.unnikrishnan@gmail.com>
2017-03-28 09:17:07 +0530

    Change Model

--- ProgrammerProfile/forms.py
+++ ProgrammerProfile/forms.py
@@ -6,5 +6,5 @@ from django import forms
 
 class Add(forms.Form):
     Username = forms.CharField(label='Your name', max_length=100,initial=" ")
-    Password=forms.CharField(widget=forms.PasswordInput,label="password",initial=" ")
+    Password=forms.CharField(widget=forms.PasswordInput,label="Password",initial=" ")
     Email = forms.EmailField(label="Email Id:", initial="",)
--- ProgrammerProfile/views.py
+++ ProgrammerProfile/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.models import User #Import the User module
 from django.contrib.auth import authenticate,login,logout#import some more stuff
 from .forms import Add
+from .models import *
 
 def index(request):
     return render(request, "ProgrammerProfile/index.html", {})
@@ -9,6 +10,7 @@ def index(request):
 def signup(request):
     form=Add()
     context={"form":form}
+
     return render(request, "ProgrammerProfile/form.html",context)
 
 
